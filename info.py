b8270d81454e14d672d4d61a0b1260b36e43e33a
siddhant012 <47074790+siddhant012@users.noreply.github.com>
2020-08-17 03:17:51 +0530

    Add files via upload

---
+++ info.py
@@ -0,0 +1,3 @@
+mode="coco"  #coco or mpi
+path="H:/visual studio code workspaces/Github Pose Estimation/"   #path to the loacl directory where this file has been kept
+display_shape=(300,300)     #webcam display shape
