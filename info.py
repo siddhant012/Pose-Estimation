63958396ff9dd32c54f754210e7697f69641ba1c
siddhant012 <47074790+siddhant012@users.noreply.github.com>
2020-08-17 11:23:59 +0530

    Delete info.py

--- info.py
+++
@@ -1,3 +0,0 @@
-mode="coco"  #coco or mpi
-path="H:/visual studio code workspaces/Github Pose Estimation/"   #path to the loacl directory where this file has been kept
-display_shape=(300,300)     #webcam display shape
